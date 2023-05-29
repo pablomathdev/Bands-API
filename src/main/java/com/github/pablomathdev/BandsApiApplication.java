ed3a1744045409976fddb519ef9e9ab12fde8f81
Pablo Matheus <pablomatheus171@gmail.com>
2023-05-29 16:09:03 -0400

    Initialize Project

---
+++ src/main/java/com/github/pablomathdev/BandsApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.pablomathdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/pablomathdev/BandsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pablomathdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
