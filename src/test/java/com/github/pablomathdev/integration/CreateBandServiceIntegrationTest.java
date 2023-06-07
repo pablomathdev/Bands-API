efe63c347d5c3c39860a5e230f30f419d1071fb0
Pablo Matheus <pablomatheus171@gmail.com>
2023-06-07 08:51:35 -0400

    test(CreateBandService): should_ThrowGenreNotFoundException_WhenGenreNotExists

--- src/test/java/com/github/pablomathdev/integration/CreateBandServiceIntegrationTest.java
+++ src/test/java/com/github/pablomathdev/integration/CreateBandServiceIntegrationTest.java
@@ -5,6 +5,7 @@ import static com.github.pablomathdev.Factory.genreFactory;
 import static com.github.pablomathdev.Factory.originFactory;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -18,8 +19,7 @@ import com.github.pablomathdev.application.services.CreateBandService;
 import com.github.pablomathdev.domain.entities.Band;
 import com.github.pablomathdev.domain.entities.Genre;
 import com.github.pablomathdev.domain.entities.Origin;
-
-
+import com.github.pablomathdev.domain.exceptions.GenreNotFoundException;
 
 @SpringBootTest
 @TestPropertySource("/application-test.properties")
@@ -40,7 +40,24 @@ public class CreateBandServiceIntegrationTest {
 		Band bandSaved = createBandService.execute(band);
 
 		assertNotNull(bandSaved.getId());
-		assertEquals(bandSaved.getName(),band.getName());
+		assertEquals(bandSaved.getName(), band.getName());
+
+	}
+
+	@Test
+	public void should_ThrowGenreNotFoundException_WhenGenreNotExists() {
+		Origin origin = originFactory("Aberdeen", "United States", 1987);
+		Genre genre = genreFactory("Grunge");
+		Set<Genre> genres = new HashSet<>();
+		genres.add(genre);
+
+		Band band = bandFactory("Nirvana", origin, genres);
+
+		Throwable exception = assertThrows(GenreNotFoundException.class, () -> {
+			createBandService.execute(band);
+		});
+		
+		assertEquals(String.format("Genre %s Not Found!",genre.getName()), exception.getMessage());
 
 	}
 
