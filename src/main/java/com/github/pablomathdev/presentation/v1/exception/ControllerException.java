9a2eb7e6efa8fa1a1194779bff7f74077f20f21c
Pablo Matheus <pablomatheus171@gmail.com>
2023-06-16 23:37:37 -0400

    initial commit

---
+++ src/main/java/com/github/pablomathdev/presentation/v1/exception/ControllerException.java
@@ -0,0 +1,9 @@
+package com.github.pablomathdev.presentation.v1.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ControllerException extends ResponseEntityExceptionHandler  {
+
+}
---
+++ src/main/java/com/github/pablomathdev/presentation/v1/exception/ErrorResponseDTO.java
@@ -0,0 +1,6 @@
+package com.github.pablomathdev.presentation.v1.exception;
+
+
+public class ErrorResponseDTO {
+
+}
