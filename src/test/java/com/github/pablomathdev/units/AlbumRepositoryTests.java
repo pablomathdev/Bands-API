111df7b6638edeb5113d1a31167c49804669d141
Pablo Matheus <pablomatheus171@gmail.com>
2023-06-22 10:09:19 -0400

    test: should_ThrowNoResultException_WhenTypedQueryThrowsNoResultException

--- src/test/java/com/github/pablomathdev/units/AlbumRepositoryTests.java
+++ src/test/java/com/github/pablomathdev/units/AlbumRepositoryTests.java
@@ -6,6 +6,7 @@ import static com.github.pablomathdev.Factory.genreFactory;
 import static com.github.pablomathdev.Factory.originFactory;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
@@ -26,9 +27,11 @@ import com.github.pablomathdev.domain.entities.Band;
 import com.github.pablomathdev.domain.entities.Genre;
 import com.github.pablomathdev.domain.entities.Origin;
 import com.github.pablomathdev.domain.entities.Track;
+import com.github.pablomathdev.domain.exceptions.notFoundExceptions.EntityNotFoundException;
 import com.github.pablomathdev.infraestructure.repositories.AlbumRepositoryImpl;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
 import jakarta.persistence.TypedQuery;
 
 @ExtendWith(MockitoExtension.class)
@@ -141,6 +144,21 @@ public class AlbumRepositoryTests {
 	  Album result = albumRepositoryImpl.findByName(album.getTitle());
 
 		assertEquals(album.getTitle(), result.getTitle());
+
+	}
+	@Test
+	public void should_ThrowNoResultException_WhenTypedQueryThrowsNoResultException() {
+		Genre genre = genreFactory("any_genre");
+		Origin origin = originFactory("any_city", "any_country", 1999);
+		Band band = bandFactory("any_name", origin, List.of(genre));
+		Album album = albumFactory("any_title", band, List.of(genre), LocalDate.parse("1999-09-09"),
+				List.of(new Track()));
+
+		when(entityManager.createQuery(SELECT_ALBUM_BY_NAME, Album.class)).thenReturn(typedQueryAlbum);
+
+		when(typedQueryAlbum.getSingleResult()).thenThrow(NoResultException.class);
+
+		assertThrows(EntityNotFoundException.class, () -> albumRepositoryImpl.findByName(album.getTitle()));
 
 	}
 }
