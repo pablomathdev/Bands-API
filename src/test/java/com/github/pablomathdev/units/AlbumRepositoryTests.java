32c45d335eaf425724b3378507d803195f97495b
Pablo Matheus <pablomatheus171@gmail.com>
2023-06-22 09:29:41 -0400

    test: should_ReturnBand_WhenEntityManagerPersistSaveBand

--- src/test/java/com/github/pablomathdev/units/AlbumRepositoryTests.java
+++ src/test/java/com/github/pablomathdev/units/AlbumRepositoryTests.java
@@ -4,7 +4,7 @@ import static com.github.pablomathdev.Factory.albumFactory;
 import static com.github.pablomathdev.Factory.bandFactory;
 import static com.github.pablomathdev.Factory.genreFactory;
 import static com.github.pablomathdev.Factory.originFactory;
-import static com.github.pablomathdev.Factory.trackFactory;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 
@@ -29,26 +29,36 @@ import jakarta.persistence.EntityManager;
 @ExtendWith(MockitoExtension.class)
 public class AlbumRepositoryTests {
 
-	
 	@Mock
 	private EntityManager entityManager;
-	
+
 	@InjectMocks
 	private AlbumRepositoryImpl albumRepositoryImpl;
-	
-	
+
 	@Test
 	public void should_InvokeEntityManagerPersist_WithCorrectArguments() {
 		Genre genre = genreFactory("any_genre");
 		Origin origin = originFactory("any_city", "any_country", 1999);
-		Band band = bandFactory("any_name",origin, List.of(genre));
-		Album album = albumFactory("any_title",band,List.of(genre),LocalDate.parse("1999-09-09"), List.of(new Track()));
-		
-		
-		
+		Band band = bandFactory("any_name", origin, List.of(genre));
+		Album album = albumFactory("any_title", band, List.of(genre), LocalDate.parse("1999-09-09"),
+				List.of(new Track()));
+
 		albumRepositoryImpl.save(album);
-		
-		
+
 		verify(entityManager).persist(eq(album));
+	}
+
+	@Test
+	public void should_ReturnBand_WhenEntityManagerPersistSaveBand() {
+		Genre genre = genreFactory("any_genre");
+		Origin origin = originFactory("any_city", "any_country", 1999);
+		Band band = bandFactory("any_name", origin, List.of(genre));
+		Album album = albumFactory("any_title", band, List.of(genre), LocalDate.parse("1999-09-09"),
+				List.of(new Track()));
+
+		Album expected = albumRepositoryImpl.save(album);
+
+		assertEquals(expected, album);
+
 	}
 }
