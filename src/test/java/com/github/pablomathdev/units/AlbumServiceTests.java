7b10727c1f1744fe69271f26752fddc41d85474f
Pablo Matheus <pablomatheus171@gmail.com>
2023-06-24 12:14:28 -0400

    test(AlbumServiceTests): should_ReturnResultListEmpty_WhenAlbumsNotExists

--- src/test/java/com/github/pablomathdev/units/AlbumServiceTests.java
+++ src/test/java/com/github/pablomathdev/units/AlbumServiceTests.java
@@ -7,6 +7,7 @@ import static com.github.pablomathdev.Factory.originFactory;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
@@ -167,6 +168,22 @@ public class AlbumServiceTests {
 	  List<Album> result = albumService.findAll();
 				
 	   assertFalse(result.isEmpty());
+
+	}
+	@Test
+	public void should_ReturnResultListEmpty_WhenAlbumsNotExists() {
+
+		Genre genre = genreFactory("any_genre");
+		Origin origin = originFactory("any_city", "any_country", 1999);
+		Band band = bandFactory("any_name", origin, List.of(genre));
+		Album album = albumFactory("any_title", band, List.of(genre), LocalDate.parse("1999-09-09"),
+				List.of(new Track()));
+
+		when(albumRepository.findAll()).thenReturn(List.of());
+		
+	  List<Album> result = albumService.findAll();
+				
+	   assertTrue(result.isEmpty());
 
 	}
 
